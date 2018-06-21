a7442bab1d9e80312d8ed728b5995ae479a1420e
kanthakumar <kanthakumar2010@gmail.com>
2018-06-21 16:40:11 +0530

    Add TestSuit api

--- testSuite.py
+++ testSuite.py
@@ -1,6 +1,7 @@
 from testrail.client import TestRail
 client = TestRail('audible-testrail.aka.amazon.com', 'ayshwarb@amazon.com', 'Bala@1962')
-caseId = '00000000' # Enter your caseId here inside '####' 
 
-result = client.get_case(caseId)
-print(result)
+suiteId = '00000000' # Enter your Suite-Id here inside '####' 
+
+case = client.get_suite(suiteId)
+print(case)
