a25c4aa0ce51e4e732512c0444ee5e069b74e150
kanthakumar <kanthakumar2010@gmail.com>
2018-06-21 17:27:45 +0530

    Unknown

--- testSuite.py
+++ testSuite.py
@@ -1,4 +1,5 @@
 from testrail.client import TestRail
+# if u have to hit someother URL change URL & credintials below.
 client = TestRail('audible-testrail.aka.amazon.com', 'ayshwarb@amazon.com', 'Bala@1962')
 
 suiteId = '00000000' # Enter your Suite-Id here inside '####' 
