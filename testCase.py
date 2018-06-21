02d1d01c4109b48239a91fdb7cde6ce6e40bc756
kanthakumar <kanthakumar2010@gmail.com>
2018-06-21 16:05:43 +0530

    add git_case

---
+++ testCase.py
@@ -0,0 +1,6 @@
+from testrail.client import TestRail
+client = TestRail('audible-testrail.aka.amazon.com', 'ayshwarb@amazon.com', 'Bala@1962')
+caseId = '00000000' # Enter your caseId here inside '####' 
+
+case = client.get_case(caseId)
+print(case)
